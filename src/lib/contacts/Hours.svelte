bc17d0858d56934787a37613029e357fde843a2c
legin8 <maynne1@student.op.ac.nz>
2022-12-20 09:59:38 +1300

    Added content to the hours component, all main content now added from the original draft site

---
+++ src/lib/contacts/Hours.svelte
@@ -0,0 +1,12 @@
+<script>
+	const openTime = "8:30am", closeTime = "2pm";
+	const firstDay = "Tuesday", lastDay = "Thursday";
+</script>
+
+<div>
+	<ul>
+		<li>Office hours</li>
+		<li>{firstDay}-{lastDay}</li>
+		<li>{openTime}-{closeTime}</li>
+	</ul>
+</div>
