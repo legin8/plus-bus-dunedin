6a6e7deeb60bd96789f1da527ae147c7a8b191e7
legin8 <maynne1@student.op.ac.nz>
2022-12-19 13:25:44 +1300

    removed almost all of the template code and added a component with the picture of the van under the heading of the website with some styles, Added new component with the text and heading for what we do

---
+++ src/lib/Intro.svelte
@@ -0,0 +1,19 @@
+<script>
+  
+</script>
+
+<div>
+  <h1 class="pageTitle">Plus Bus Dunedin</h1>
+  <img src="plusBusVan.jpg" class="vanPic" alt="Van">
+</div>
+
+<style>
+  .vanPic {
+    width: 50vw;
+    height: 50vh;
+  }
+
+  .pageTitle {
+    font-size: 54px;
+  }
+</style>
