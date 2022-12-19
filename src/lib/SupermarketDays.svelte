76abab5df08b6d11f826aab958cd56d2e85f4812
legin8 <maynne1@student.op.ac.nz>
2022-12-19 18:42:16 +1300

    refactored the SupermarketDays component to use a for Each loop with an array of objects currently in the same file

--- src/lib/SupermarketDays.svelte
+++ src/lib/SupermarketDays.svelte
@@ -1,25 +1,18 @@
 <script>
-  
+  const table = [{day: "Tuesday Mornings", supermarket: "Countdown Mornington", area: "Mornington/Hill suburbs"},
+{day: "Thursday Mornings", supermarket: "Pak 'n' Save", area: "South Dunedin, Anderson's Bay, St Clair"},
+{day: "Thursday Afternoons", supermarket: "Countdown Andersons Bay", area: "South Dunedin, Anderson's Bay and Waverley"}];
 </script>
 
-<div class="runTable">
-  <div>
-    <p>Tuesday Mornings</p>
-    <p>Thursday Mornings</p>
-    <p>Thursday Afternoons</p>
-  </div>
 
-  <div>
-    <p>Countdown Mornington</p>
-    <p>Pak 'n' Save</p>
-    <p>Countdown Andersons Bay</p>
-  </div>
-
-  <div>
-    <p>Mornington/Hill suburbs</p>
-    <p>South Dunedin, Anderson's Bay, St Clair</p>
-    <p>South Dunedin, Anderson's Bay and Waverley</p>
-  </div>
+<div>
+  <ul class="runTable">
+    {#each table as entry}
+      <li>{entry.day}</li>
+      <li>{entry.supermarket}</li>
+      <li>{entry.area}</li>
+    {/each}
+  </ul>
   
 </div>
 
