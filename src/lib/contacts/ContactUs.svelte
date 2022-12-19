95849ab5edb96e1fcb41e04317d6b954ec8f218d
legin8 <maynne1@student.op.ac.nz>
2022-12-20 09:50:39 +1300

    added content to the contactus component

---
+++ src/lib/contacts/ContactUs.svelte
@@ -0,0 +1,24 @@
+<script>
+	// This array contains the address in the order of street, suburb, city, post code.
+	const address = ["2 Playfair Street", "Caversham", "Dunedin", "9012"]
+</script>
+
+<div>
+	<h2>Contact us</h2>
+	<div>
+		<p>03 455 4050 (Co-ordinator)</p>
+		<a href="mailto:plusbusshops@gmail.com?subject=Plus Bus Inquirers">
+			<em>
+				plusbusshops@gmail.com
+			</em>
+		</a>
+	</div>
+	<div>
+		<ul>
+			<li>{address[0]},</li>
+			<li>{address[1]},</li>
+			<li>{address[2]},</li>
+			<li>{address[3]}.</li>
+		</ul>
+	</div>
+</div>
