ed45d275598f6ae819d7bfd5dfa79c656fa23320
legin8 <maynne1@student.op.ac.nz>
2022-12-21 12:35:58 +1300

    Added onMount function to the supermarketDays component and changed the background color and did general tidy up of the content up untill the supermarket component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script>
+  // Components imported here from the lib directory
   import Intro from "./lib/Intro.svelte";
   import DaysCost from "./lib/Days-Cost.svelte";
   import ContactHours from "./lib/Contact-Hours.svelte";
 </script>
 
 <main class="appMain">
-
   <div>
     <Intro />
   </div>
