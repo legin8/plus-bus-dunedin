bbdcd701a2b7fefb0cb02102163a2826c90aeebf
legin8 <maynne1@student.op.ac.nz>
2023-02-14 07:28:23 +1300

    changed the vite.config file base: to new custom domain, that fixed the bug stopping the website from loading

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/plus-bus-dunedin/",
+  base: "https://plusbusdunedin.co.nz/",
   plugins: [svelte()],
 })
